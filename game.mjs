5071f881273795f9fcdd6a6e8c109f0e513006b0
RazperSurp <dshiyanov01@gmail.com>
2024-11-28 01:14:09 +0300

    difficulty presets wip

--- game.mjs
+++ game.mjs
@@ -1,36 +1,221 @@
 export default class Game {
     _DIFFICULTY_PRESETS = [
         { // легкая
-            WAVE_DURATION: 10,
+            WAVE_DURATION_BASE: 10,
+            WAVE_DURATION_INCREMENT_PER_WAVE: 0.5,
             SPAWN_INTERVAL: 1,
             SPAWN_CHANCE: 0.6,
             DOUBLE_SPAWN_CHANCE: 0.05,
             ENEMIES: {
-                // LIGHT: 
+                HH: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, HM: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, HL: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, MH: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, MM: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, ML: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, LH: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, LM: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, LL: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }
             }
         }, { // нормальная
-            WAVE_DURATION: 25,
+            WAVE_DURATION_BASE: 15,
+            WAVE_DURATION_INCREMENT_PER_WAVE: 1,
             SPAWN_INTERVAL: 0.9,
             SPAWN_CHANCE: 0.75,
             DOUBLE_SPAWN_CHANCE: 0.2,
             ENEMIES: {
-                // LIGHT: 
+                HH: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 7,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 3,
+                    SPAWN_CHANCE_MAX: 40,
+                }, HM: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, HL: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, MH: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, MM: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, ML: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, LH: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, LM: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, LL: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                },
             }
         }, {  // сложная
-            WAVE_DURATION: 40,
+            WAVE_DURATION_BASE: 20,
+            WAVE_DURATION_INCREMENT_PER_WAVE: 1.5,
             SPAWN_INTERVAL: 0.7,
             SPAWN_CHANCE: 0.8,
             DOUBLE_SPAWN_CHANCE: 0.35,
             ENEMIES: {
-                // LIGHT: 
+                HH: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 9,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 5,
+                    SPAWN_CHANCE_MAX: 50,
+                }, HM: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, HL: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, MH: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, MM: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, ML: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, LH: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, LM: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, LL: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                },
             }
         }, { // кошмар
-            WAVE_DURATION: 60,
+            WAVE_DURATION_BASE: 25,
+            WAVE_DURATION_INCREMENT_PER_WAVE: 2,
             SPAWN_INTERVAL: 0.5,
             SPAWN_CHANCE: 0.95,
             DOUBLE_SPAWN_CHANCE: 0.5,
             ENEMIES: {
-                // LIGHT: 
+                HH: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 11,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 7,
+                    SPAWN_CHANCE_MAX: 60,
+                }, 
+                HM: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, HL: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, MH: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, MM: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, ML: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, LH: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, LM: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                }, LL: {
+                    MIN_WAVE: 0,
+                    SPAWN_CHANCE_BASE: 5,
+                    SPAWN_CHANCE_INCREMENT_PER_WAVE: 1,
+                    SPAWN_CHANCE_MAX: 30,
+                },
             }
         }
     ]
