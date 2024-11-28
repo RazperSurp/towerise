2162229c0cfcdc42055ba1fbb9778c03bd3b5185
= <=>
2024-11-28 14:42:59 +0300

    enemy, tower, status and status resist. xD

---
+++ tower.mjs
@@ -0,0 +1,11 @@
+class Tower{
+    constructor(towerName, attackSpeed, damage, status, multishot, asset, cost){
+        this.TOWERNAME = towerName
+        this.ATTACKSPEED = attackSpeed
+        this.DAMAGE = damage
+        this.STATUS = status
+        this.MULTISHOT = multishot
+        this.ASSET = asset
+        this.COST = cost
+    }
+}
