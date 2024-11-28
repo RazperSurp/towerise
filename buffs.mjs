1a142f734ad349f1d47cf3f9281ed5c296a45750
Kret1n <maksimlitovcenko11@gmail.com>
2024-11-28 16:03:02 +0300

    new time in buffs

--- buffs.mjs
+++ buffs.mjs
@@ -2,21 +2,21 @@ class Buff{
     BUFF = {
         FIRE: {
             DAMAGE: 999,
-            TIME: 2,
+            TIME: 2000,
         },
         SLOWDOWN: {
             DAMAGE: 0,
             REDUCING_MOVEMENT_SPEED: 0.4,
-            TIME: 5,
+            TIME: 5000,
         },
         STUN: {
             DAMAGE: 0,
             STUN_TIME: 1,
-            TIME: 3,
+            TIME: 3000,
         },
         POISON: {
             DAMAGE: 1,
-            TIME: 40,
+            TIME: 40000,
         }
     }   
 }
