30b95a1cdbaa4f045123b03fb6283634dc6b31a7
= <=>
2024-11-28 16:14:45 +0300

    more assets, improved randomizing of assets to tile

--- game.mjs
+++ game.mjs
@@ -271,8 +271,70 @@ export default class Game {
                 const CELL = document.createElement('td');
                 CELL.dataset.addr = x;
 
-                const ASSET_INDEX = Math.ceil(Math.random() * 5);
-                CELL.classList.add(`grass-${ASSET_INDEX}`);
+                const ASSET_INDEX = Math.ceil(Math.random() * 40);
+                let asset = '';
+
+                switch (ASSET_INDEX) {
+                    case 1:
+                    case 10:
+                    case 15:
+                    case 23:
+                    case 28:
+                    case 33:
+                    case 38:
+                        asset = `url(./assets/grass-1.svg)`;
+                        break;
+                    case 2:
+                    case 11:
+                    case 16:
+                    case 24:
+                    case 29:
+                    case 34:
+                    case 39:
+                        asset = `url(./assets/grass-2.svg)`;
+                        break;
+                    case 3:
+                    case 12:
+                    case 17:
+                    case 25:
+                    case 30:
+                    case 35:
+                    case 40:
+                        asset = `url(./assets/grass-3.svg)`;
+                        break;
+                    case 4:
+                    case 13:
+                    case 18:
+                    case 26:
+                    case 31:
+                    case 36:
+                        asset = `url(./assets/grass-4.svg)`;
+                        break;
+                    case 5:
+                    case 14:
+                    case 19:
+                    case 27:
+                    case 32:
+                    case 37:
+                        asset = `url(./assets/grass-5.svg)`;
+                        break;
+                    case 20:
+                        asset = `url(./assets/grass-10.svg)`;
+                        break;
+                    case 21:
+                        asset = `url(./assets/grass-11.svg)`;
+                        break;
+                    case 22:
+                        asset = `url(./assets/grass-12.svg)`;
+                        break;
+
+                    default:
+                        asset = `url(./assets/grass-${ASSET_INDEX}.svg)`;
+                        break;
+                    
+                }
+
+                CELL.style.backgroundImage = asset;
 
                 ROW.appendChild(CELL);
 
